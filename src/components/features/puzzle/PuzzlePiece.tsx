a19ef4361628a6d87b3294dea3c559a36eb2541b
NiniMerabishvili <ninni.merabishvili@gmail.com>
2025-12-05 17:06:08 +0400

    Finish

--- src/components/features/puzzle/PuzzlePiece.tsx
+++ src/components/features/puzzle/PuzzlePiece.tsx
@@ -12,9 +12,9 @@ export const PuzzlePiece = ({ piece }: PuzzlePieceProps) => {
         alt={`Puzzle piece ${piece.id}`}
         className="w-full h-full object-cover block"
       />
-      <div className="absolute top-1 left-1 bg-white text-black text-xs font-black px-1.5 py-0.5 border border-black">
+      {/* <div className="absolute top-1 left-1 bg-white text-black text-xs font-black px-1.5 py-0.5 border border-black">
         {piece.id}
-      </div>
+      </div> */}
     </div>
   );
 };
